a72445ebaacaf275f9d89a257f33d6e353488583
Andreas Fagerlund <mcshaffa@gmail.com>
2025-09-02 11:49:56 +0200

    made header. made test

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import { db } from "@/prisma/db";
-import TodoList from "./ui/todo-list";
+import { db } from '@/prisma/db';
+import Header from './ui/header';
+import TodoList from './ui/todo-list';
 
 export default async function Home() {
   const todos = await db.todo.findMany();
 
   return (
     <main>
+      <Header />
       <TodoList defaultTodos={todos} />
     </main>
   );
